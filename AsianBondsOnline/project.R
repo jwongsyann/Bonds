57f2a17d3d4a8d564b4e19f09d358ffad6444577
jwongsyann <justin.jwsy@gmail.com>
2015-05-27 17:28:01 +0800

    Finalize ABO script
    Changed the format to make it neat and tidy data. Variables via columns,
    observations via rows.

--- AsianBondsOnline/project.R
+++ AsianBondsOnline/project.R
@@ -12,70 +12,87 @@ tradingVolume<-"http://asianbondsonline.adb.org/spreadsheets/RG-Trading_Volume.x
 lcybond<-downloadABO(lcybond)
 lcybond<-cleanABO(lcybond)
 lcybond<-reshapeABO(lcybond)
-library(reshape2)
-lcybond<-dcast(lcybond,Date+Market+Currency~Entity)
-names(lcybond)<-c("Date","Market","Currency","BOND001F001","BOND001F002","BOND001F003")
+lcybond<-data.frame(lcybond,Variable=rep("Size of LCY Bond Market",nrow(lcybond)))
+lcybond<-lcybond[,c("Date","Market","Entity","Currency","Variable","value")]
 
 # FCY_Bonds_Outstanding
 fcybond<-downloadABO(fcybond)
 fcybond<-cleanABO(fcybond)
 fcybond<-reshapeABO(fcybond)
-library(reshape2)
-fcybond<-dcast(fcybond,Date+Market+Currency~Entity)
-names(fcybond)<-c("Date","Market","Currency","BOND002F001","BOND002F002","BOND002F003","BOND002F004")
+fcybond<-data.frame(fcybond,Variable=rep("Size of FCY Bond Market",nrow(fcybond)))
+fcybond<-fcybond[,c("Date","Market","Entity","Currency","Variable","value")]
 
 # Issuance_Volume_LCY
 issuanceLCY<-downloadABO(issuanceLCY)
 issuanceLCY<-cleanABO(issuanceLCY)
 issuanceLCY<-reshapeABO(issuanceLCY)
-library(reshape2)
-issuanceLCY<-dcast(issuanceLCY,Date+Market+Currency~Entity)
-names(issuanceLCY)<-c("Date","Market","Currency","BOND003F001","BOND003F002","BOND003F003")
+issuanceLCY<-data.frame(issuanceLCY,Variable=rep("Issuance Volume of LCY Bond Market",nrow(issuanceLCY)))
+issuanceLCY<-issuanceLCY[,c("Date","Market","Entity","Currency","Variable","value")]
+
 
 # G3_Currency_Bond_Issuance
 # Do not need to reshape as there are no factors in this dataset
 issuanceG3<-downloadABO(issuanceG3)
 issuanceG3<-cleanABO(issuanceG3)
 issuanceG3<-reshapeABO(issuanceG3)
-library(reshape2)
-issuanceG3<-dcast(issuanceG3,Date+Market+Currency~Entity)
-names(issuanceG3)<-c("Date","Market","Currency","BOND004F001")
+issuanceG3<-data.frame(issuanceG3,Variable=rep("Issuance Volume of G3 Currency Bond Market",nrow(issuanceG3)))
+issuanceG3<-issuanceG3[,c("Date","Market","Entity","Currency","Variable","value")]
 
 # Bond_turn_ratio
 bondturnover<-downloadABO(bondturnover)
 bondturnover<-cleanABO(bondturnover)
 library(reshape2)
-temp<-melt(bondturnover,id.vars=c("Date","Market"),variable.name="column")
+bondturnover<-melt(bondturnover,id.vars=c("Date","Market"),variable.name="Variable")
 # Initializes the Currency Column 
-temp$Currency<-rep("USD",nrow(bondturnover))
+bondturnover$Currency<-rep("USD",nrow(bondturnover))
 # Finds the data reported by LCY and updates the currency column
-temp$Currency[grep("LCY",bondturnover$Entity)]<-"LCY"
-bondturnover<-cbind(bondturnover,temp$Currency)
-bondturnover<-bondturnover[,c(1,2,9,3,4,5,6,7,8)]
-names(bondturnover)<-c("Date","Market","Currency","BOND006F001","BOND006F002","BOND006F003","BOND006F004","BOND006F005","BOND006F006")
+bondturnover$Currency[grep("LCY",bondturnover$Entity)]<-"LCY"
+# Changes the Entity Class into Character from factor.
+bondturnover$Variable<-as.character(bondturnover$Variable)
+bondturnover$Entity<-as.character(bondturnover$Variable)
+# Replaces the Entity with the cleaned up names.
+bondturnover$Entity[grep("Gov",bondturnover$Entity)]<-"Government"
+bondturnover$Entity[grep("Banks",bondturnover$Entity)]<-"BanksAndFinancialInstituitions"
+bondturnover$Entity[grep("Corp",bondturnover$Entity)]<-"NonFinancialCorporates"
+bondturnover$Entity[grep("Total",bondturnover$Entity)]<-"Total"
+bondturnover$Entity[grep("Foreign",bondturnover$Entity)]<-"ForeignHoldings"
+# Replaces the Variable with the cleaned up names.
+bondturnover$Variable[grep("Ratio",bondturnover$Variable)]<-"Bond Turnover Ratio"
+bondturnover$Variable[grep("Ave",bondturnover$Variable)]<-"Average Bonds Outstanding"
+bondturnover$Variable[grep("Turnover..in",bondturnover$Variable)]<-"Bond Turnover"
+bondturnover<-bondturnover[,c("Date","Market","Entity","Currency","Variable","value")]
 
 # Trading_Volume
 tradingVolume<-downloadABO(tradingVolume)
 tradingVolume<-cleanABO(tradingVolume)
 tradingVolume<-reshapeABO(tradingVolume)
-library(reshape2)
-tradingVolume<-dcast(tradingVolume,Date+Market+Currency~Entity)
-names(tradingVolume)<-c("Date","Market","Currency","BOND007F001","BOND007F002","BOND007F003")
+tradingVolume<-data.frame(tradingVolume,Variable=rep("Trading Volume",nrow(tradingVolume)))
+tradingVolume<-tradingVolume[,c("Date","Market","Entity","Currency","Variable","value")]
 
 # foreignHoldingsofGovtLCY
 foreignHoldingsofGovtLCY<-downloadABO(foreignHoldingsofGovtLCY)
 foreignHoldingsofGovtLCY<-cleanABO(foreignHoldingsofGovtLCY)
 library(reshape2)
-temp<-melt(foreignHoldingsofGovtLCY,id.vars=c("Date","Market"),variable.name="column")
+foreignHoldingsofGovtLCY<-melt(foreignHoldingsofGovtLCY,id.vars=c("Date","Market"),variable.name="Variable")
 # Initializes the Currency Column 
-temp$Currency<-rep("USD",nrow(foreignHoldingsofGovtLCY))
+foreignHoldingsofGovtLCY$Currency<-rep(NA,nrow(foreignHoldingsofGovtLCY))
 # Finds the data reported by LCY and updates the currency column
-temp$Currency[grep("LCY",temp$column)]<-"LCY"
-foreignHoldingsofGovtLCY<-cbind(foreignHoldingsofGovtLCY,temp$Currency)
-foreignHoldingsofGovtLCY<-foreignHoldingsofGovtLCY[,c(1,2,6,3,4,5)]
-names(foreignHoldingsofGovtLCY)<-c("Date","Market","Currency","BOND008F001","BOND008F002","BOND008F003")
+foreignHoldingsofGovtLCY$Currency[grep("LCY",foreignHoldingsofGovtLCY$Variable)]<-"LCY"
+# Changes the Entity Class into Character from factor.
+foreignHoldingsofGovtLCY$Variable<-as.character(foreignHoldingsofGovtLCY$Variable)
+foreignHoldingsofGovtLCY$Entity<-as.character(foreignHoldingsofGovtLCY$Variable)
+# Replaces the Entity with the cleaned up names.
+foreignHoldingsofGovtLCY$Entity<-"Total"
+# Replaces the Variable with the cleaned up names.
+foreignHoldingsofGovtLCY$Variable[grep("Foreign",foreignHoldingsofGovtLCY$Variable)]<-"Foreign Holdings of LCY Govt Bonds Outstanding"
+foreignHoldingsofGovtLCY$Variable[grep("of.Total",foreignHoldingsofGovtLCY$Variable)]<-"Foreign Holdings of LCY Govt Bonds Outstanding, Percent"
+foreignHoldingsofGovtLCY$Variable[grep("Total",foreignHoldingsofGovtLCY$Variable)]<-"Total LCY Govt Bonds Outstanding"
+foreignHoldingsofGovtLCY<-foreignHoldingsofGovtLCY[,c("Date","Market","Entity","Currency","Variable","value")]
+
+datABO<-rbind(lcybond,fcybond,issuanceLCY,issuanceG3,bondturnover,tradingVolume,foreignHoldingsofGovtLCY)
+datABO<-dcast(datABO,Date+Market+Entity+Currency~Variable)
 
 #url8<-"https://www.bis.org/statistics/credtopriv/credtopriv.xlsx"
 #download.file(url8,destfile="credtopriv.xlsx",mode="wb")
 #dat8<-read.xlsx("credtopriv.xlsx",sheetIndex=3,startRow=3)
-mdat<-Reduce(function(x,y){merge(x,y,all=TRUE)},list(lcybond,fcybond,issuanceLCY,issuanceG3,bondturnover,tradingVolume,foreignHoldingsofGovtLCY))
+# mdat<-Reduce(function(x,y){merge(x,y,all=TRUE)},list(lcybond,fcybond,issuanceLCY,issuanceG3,bondturnover,tradingVolume,foreignHoldingsofGovtLCY))
